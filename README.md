f21d1e94c9d3f7371d09216f61a265d7d14dc3de
lm93129 <lm93129@qq.com>
2019-08-29 08:48:19 +0800

    新增json格式的识别 新增多个测试集合的支持 更新readme文档

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 请求yapi的接口，进行测试集合的测试。使用方法如下：
-```
+```yaml
  #YAPI接口自动测试
   - name: yapi-test
     image: registry.cn-hangzhou.aliyuncs.com/lm93129/drone_yapi_test:latest
     settings:
-      url: http://yapi.com/api/open/run_auto_test?id=11&token=ab1058e0945cf14&env_11=测试环境&mode=html&email=false&download=false
+      host: http://yapi.com.cn/
+      id: [11, 31, 15]
+      token: ab1058076e0945cf14
+      env: env_11=网关地址
+      mode: json
 ```
-
-注意yapi的测试地址接口必须选择html和非下载
+使用docker运行
+```bash
+docker run --rm \
+  -e PLUGIN_HOST=http://yapi.com.cn/ \
+  -e PLUGIN_MODE=json \
+  -e PLUGIN_TOKEN=ab1058076e0945cf14 \
+  -e PLUGIN_ID=[11, 31, 15] \
+  -e PLUGIN_ENV="env_11=网关地址" \
+  -v $(pwd):/drone/src \
+  -w /drone/src \
+  registry.cn-hangzhou.aliyuncs.com/lm93129/drone_yapi_test:latest
+```
+注意：host地址后面必须接上/
--- main.go
+++ main.go
@@ -1,27 +1,100 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"regexp"
+	"strings"
+	"time"
 )
 
-func main() {
-	url := os.Getenv("PLUGIN_URL")
-	re, _ := regexp.Compile(`&mode=json`)
-	match := re.FindAllStringSubmatch(url, -1)
-	if match != nil {
-		panic("请使用html的接口")
-	}
-	fmt.Printf("YAPI地址为: %s \n", url)
-	List(url)
+type YApiJson struct {
+	Message struct {
+		Msg        string `json:"msg"`
+		Len        int    `json:"len"`
+		SuccessNum int    `json:"successNum"`
+		FailedNum  int    `json:"failedNum"`
+	} `json:"message"`
+	RunTime string `json:"runTime"`
+	Numbs   int    `json:"numbs"`
+	List    []struct {
+		ID       int    `json:"id"`
+		Name     string `json:"name"`
+		Path     string `json:"path"`
+		Code     int    `json:"code"`
+		ValidRes []struct {
+			Message string `json:"message"`
+		} `json:"validRes"`
+		Status  int    `json:"status"`
+		URL     string `json:"url"`
+		Method  string `json:"method"`
+		Headers struct {
+			ContentType string `json:"Content-Type"`
+			Accept      string `json:"accept"`
+		} `json:"headers"`
+		ResHeader struct {
+			Server           string `json:"server"`
+			Date             string `json:"date"`
+			ContentType      string `json:"content-type"`
+			TransferEncoding string `json:"transfer-encoding"`
+			Connection       string `json:"connection"`
+			Vary             string `json:"vary"`
+		} `json:"res_header"`
+		ResBody struct {
+			Code      int         `json:"code"`
+			Status    bool        `json:"status"`
+			Message   string      `json:"message"`
+			Data      interface{} `json:"data"`
+			Timestamp string      `json:"timestamp"`
+		} `json:"res_body"`
+		Params struct {
+			Username    string `json:"username"`
+			Password    string `json:"password"`
+			DeviceType  string `json:"deviceType"`
+			ContentType string `json:"Content-Type"`
+			Accept      string `json:"accept"`
+		} `json:"params"`
+	} `json:"list"`
 }
 
-func List(url string) {
-	var htmldate = html(url)
+func main() {
+	YApihost := os.Getenv("PLUGIN_HOST")
+	mode := os.Getenv("PLUGIN_MODE")
+	token := os.Getenv("PLUGIN_TOKEN")
+	id := os.Getenv("PLUGIN_ID")
+	env := os.Getenv("PLUGIN_ENV")
+	beseurl := YApihost +
+		"api/open/run_auto_test?" +
+		mode + "&" +
+		token + "&" +
+		env + "&email=false&download=false&"
+
+	checkapi(beseurl, id, mode)
+}
+
+func checkapi(baseurl string, id string, mode string) {
+	i := 0
+	for _, v := range strings.Split(id, ",") {
+		url := baseurl + v
+		if mode == "json" {
+			fmt.Printf("测试接口地址为: %s \n", url)
+			i = i + judge_json(url)
+		} else {
+			fmt.Printf("测试接口地址为: %s \n", url)
+			i = i + testapi_html(url)
+		}
+		if i > 0 {
+			log.Panic("接口测试不通过，请检查YApi和接口")
+		}
+	}
+}
+
+func testapi_html(url string) int {
+	var htmldate = getbody(url)
 	for _, m := range statistical(htmldate) {
 		fmt.Printf("%s  %s%s\n", m[1], m[2], m[3])
 	}
@@ -31,33 +104,65 @@ func List(url string) {
 	for _, stat := range statistical(htmldate) {
 		i := string(stat[2])
 		if i != "" {
-			panic("接口测试不通过，请检查YAPI和接口")
+			return 1
 		}
 	}
+	return 0
 }
 
-func html(url string) []byte {
+func getbody(url string) []byte {
 	resp, err := http.Get(url)
-	if err != nil {
-		log.Panic(err)
-	}
+	dropErr(err)
 	defer resp.Body.Close()
 	s, err := httputil.DumpResponse(resp, true)
-	if err != nil {
-		log.Panic(err)
-	}
+	dropErr(err)
 	return s
 }
 
-func getlist(htmldate []byte) [][][]byte {
+func getlist(contents []byte) [][][]byte {
 	re := regexp.MustCompile(`href="#[0-9]+">([^<]+)</a>
     <div title="([^\"]+)"`)
-	match := re.FindAllSubmatch(htmldate, -1)
+	match := re.FindAllSubmatch(contents, -1)
 	return match
 }
 
-func statistical(htmldate []byte) [][][]byte {
+func statistical(contents []byte) [][][]byte {
 	re := regexp.MustCompile(`<div class="summary"><div>([^<]+)<span class="success"> ([0-9]+)</span>([^<]+)</div>`)
-	match := re.FindAllSubmatch(htmldate, -1)
+	match := re.FindAllSubmatch(contents, -1)
 	return match
+}
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	dropErr(err)
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+func judge_json(url string) int {
+	apijs := new(YApiJson)
+	err := getJson(url, apijs)
+	if err != nil {
+		log.Panicln("json解析失败")
+	}
+	fmt.Println(apijs.Message.Msg)
+	for i := 0; i < len(apijs.List); i++ {
+		name := apijs.List[i].Name
+		message := apijs.List[i].ValidRes[0].Message
+		fmt.Printf("接口用例名称：%s , 验证结果： %s \n", name, message)
+	}
+	if apijs.Message.FailedNum != 0 {
+		fmt.Printf("接口验证不通过，错误数：%d , 耗时：%s \n", apijs.Message.FailedNum, apijs.RunTime)
+		return 1
+	}
+	return 0
+}
+
+func dropErr(e error) {
+	if e != nil {
+		log.Panic(e)
+	}
 }
