e9af7029349e4962c97b3e58eb9555d3fae73b62
lm93129 <lm93129@qq.com>
2019-08-30 17:49:06 +0800

    优化代码和函数命名

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-type YApiJson struct {
+type YApiJSON struct {
 	Message struct {
 		Msg        string `json:"msg"`
 		Len        int    `json:"len"`
@@ -62,30 +62,30 @@ type YApiJson struct {
 }
 
 func main() {
-	YApihost := os.Getenv("PLUGIN_HOST")
+	YApiHOST := os.Getenv("PLUGIN_HOST")
 	mode := os.Getenv("PLUGIN_MODE")
 	token := os.Getenv("PLUGIN_TOKEN")
 	id := os.Getenv("PLUGIN_ID")
 	env := os.Getenv("PLUGIN_ENV")
-	beseurl := YApihost +
-		"api/open/run_auto_test?" +
-		mode + "&" +
-		token + "&" +
-		env + "&email=false&download=false&"
-
-	checkapi(beseurl, id, mode)
+	BaseURL := fmt.Sprintf("%s/api/open/run_auto_test?%s&%s&%s&email=false&download=false&",
+		YApiHOST,
+		mode,
+		token,
+		env,
+	)
+	checkAPI(BaseURL, id, mode)
 }
 
-func checkapi(baseurl string, id string, mode string) {
+func checkAPI(BaseURL, id, mode string) {
 	i := 0
 	for _, v := range strings.Split(id, ",") {
-		url := baseurl + v
+		url := BaseURL + v
 		if mode == "json" {
 			fmt.Printf("测试接口地址为: %s \n", url)
-			i = i + judge_json(url)
+			i = i + judgeJSON(url)
 		} else {
 			fmt.Printf("测试接口地址为: %s \n", url)
-			i = i + testapi_html(url)
+			i = i + testAPIHTML(url)
 		}
 		if i > 0 {
 			log.Panic("接口测试不通过，请检查YApi和接口")
@@ -93,15 +93,15 @@ func checkapi(baseurl string, id string, mode string) {
 	}
 }
 
-func testapi_html(url string) int {
-	var htmldate = getbody(url)
-	for _, m := range statistical(htmldate) {
+func testAPIHTML(url string) int {
+	var HtmlDATE = getBODY(url)
+	for _, m := range statistical(HtmlDATE) {
 		fmt.Printf("%s  %s%s\n", m[1], m[2], m[3])
 	}
-	for _, m := range getlist(htmldate) {
+	for _, m := range getLIST(HtmlDATE) {
 		fmt.Printf("%s   %s\n", m[1], m[2])
 	}
-	for _, stat := range statistical(htmldate) {
+	for _, stat := range statistical(HtmlDATE) {
 		i := string(stat[2])
 		if i != "" {
 			return 1
@@ -110,7 +110,7 @@ func testapi_html(url string) int {
 	return 0
 }
 
-func getbody(url string) []byte {
+func getBODY(url string) []byte {
 	resp, err := http.Get(url)
 	dropErr(err)
 	defer resp.Body.Close()
@@ -119,7 +119,7 @@ func getbody(url string) []byte {
 	return s
 }
 
-func getlist(contents []byte) [][][]byte {
+func getLIST(contents []byte) [][][]byte {
 	re := regexp.MustCompile(`href="#[0-9]+">([^<]+)</a>
     <div title="([^\"]+)"`)
 	match := re.FindAllSubmatch(contents, -1)
@@ -134,7 +134,7 @@ func statistical(contents []byte) [][][]byte {
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func getJson(url string, target interface{}) error {
+func getJSON(url string, target interface{}) error {
 	r, err := myClient.Get(url)
 	dropErr(err)
 	defer r.Body.Close()
@@ -142,20 +142,20 @@ func getJson(url string, target interface{}) error {
 	return json.NewDecoder(r.Body).Decode(target)
 }
 
-func judge_json(url string) int {
-	apijs := new(YApiJson)
-	err := getJson(url, apijs)
+func judgeJSON(url string) int {
+	apiJSON := new(YApiJSON)
+	err := getJSON(url, apiJSON)
 	if err != nil {
 		log.Panicln("json解析失败")
 	}
-	fmt.Println(apijs.Message.Msg)
-	for i := 0; i < len(apijs.List); i++ {
-		name := apijs.List[i].Name
-		message := apijs.List[i].ValidRes[0].Message
+	fmt.Println(apiJSON.Message.Msg)
+	for i := 0; i < len(apiJSON.List); i++ {
+		name := apiJSON.List[i].Name
+		message := apiJSON.List[i].ValidRes[0].Message
 		fmt.Printf("接口用例名称：%s , 验证结果： %s \n", name, message)
 	}
-	if apijs.Message.FailedNum != 0 {
-		fmt.Printf("接口验证不通过，错误数：%d , 耗时：%s \n", apijs.Message.FailedNum, apijs.RunTime)
+	if apiJSON.Message.FailedNum != 0 {
+		fmt.Printf("接口验证不通过，错误数：%d , 耗时：%s \n", apiJSON.Message.FailedNum, apiJSON.RunTime)
 		return 1
 	}
 	return 0
