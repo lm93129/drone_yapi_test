0db956938837b025197a4dd054d57dad39b47514
lm93129 <lm93129@qq.com>
2020-01-13 14:03:09 +0800

    新增数据收集功能

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
+// PROJECT 项目id
+// DATAURL 数据收集平台的地址
+// post请求头会带上项目的id
+
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/joho/godotenv"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -32,12 +39,16 @@ func CheckApi(BaseUrl, id string) {
 	l := strings.Split(id, ",")
 	c := make(chan int, len(l))
 	v := 0
+
+	// 启动多线程遍历请求每一个用例集合
 	for _, v := range l {
 		url := BaseUrl + v
 		go func(u, v string) {
 			YapiAutoTest(u, v, c)
 		}(url, v)
 	}
+
+	// 打印每一个用例集合
 	for i := 0; i < len(l); i++ {
 		tm := time.NewTimer(time.Second * 20)
 		select {
@@ -47,6 +58,9 @@ func CheckApi(BaseUrl, id string) {
 			log.Println("测试超时，请检查网络环境")
 		}
 	}
+
+	// 做个统计，在所有接口测试完毕之后统计是否有不通过的测试用例集合
+	// 如果有不通过的用例就会报错跳出
 	if v > 0 {
 		log.Panic("接口测试不通过，请检查")
 	}
@@ -59,8 +73,14 @@ func YapiAutoTest(url, v string, c chan int) {
 	r, err := myClient.Get(url)
 	dropErr(err)
 	defer r.Body.Close()
+
 	_ = json.NewDecoder(r.Body).Decode(apiJSON)
 	log.Printf("开始测试ID为 %s 的用例集合 ", v)
+	apijsondata, err := json.Marshal(apiJSON)
+	qaurl := os.Getenv("DATAURL")
+	if qaurl != "" && err == nil {
+		qapost(qaurl, apijsondata)
+	}
 	log.Println(apiJSON.Message.Msg)
 
 	for i := 0; i < len(apiJSON.List); i++ {
@@ -69,16 +89,35 @@ func YapiAutoTest(url, v string, c chan int) {
 		log.Printf("接口用例名称：%s , 验证结果： %s \n", name, message)
 	}
 	if apiJSON.Message.FailedNum != 0 {
-		log.Printf("接口验证不通过，错误数：%d , 耗时：%s \n", apiJSON.Message.FailedNum, apiJSON.RunTime)
+		log.Printf("接口验证不通过，错误数：%d , 用例集合耗时：%s \n", apiJSON.Message.FailedNum, apiJSON.RunTime)
 		c <- 1
 	}
 	c <- 0
+	//发送数据到数据收集平台
+
 }
 
+// 处理错误
 func dropErr(e error) {
 	if e != nil {
+		log.Println("出现异常错误，错误如下")
 		log.Panic(e)
 	}
+}
+
+// 发送测试过程的数据到质量平台
+func qapost(url string, body []byte) {
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("project_id", os.Getenv("PROJECT"))
+	resp, err := myClient.Do(req)
+	// 这里出错不跳出程序，避免qa平台失效导致的意外退出
+	if err != nil {
+		log.Println(err)
+	}
+	s, _ := ioutil.ReadAll(resp.Body)
+	log.Printf("数据收集情况：%s", s)
 }
 
 // YApiJSON 返回的json序列化
@@ -106,27 +145,8 @@ type YApiJSON struct {
 			ContentType string `json:"Content-Type"`
 			Accept      string `json:"accept"`
 		} `json:"headers"`
-		ResHeader struct {
-			Server           string `json:"server"`
-			Date             string `json:"date"`
-			ContentType      string `json:"content-type"`
-			TransferEncoding string `json:"transfer-encoding"`
-			Connection       string `json:"connection"`
-			Vary             string `json:"vary"`
-		} `json:"res_header"`
-		ResBody struct {
-			Code      int         `json:"code"`
-			Status    bool        `json:"status"`
-			Message   string      `json:"message"`
-			Data      interface{} `json:"data"`
-			Timestamp string      `json:"timestamp"`
-		} `json:"res_body"`
-		Params struct {
-			Username    string `json:"username"`
-			Password    string `json:"password"`
-			DeviceType  string `json:"deviceType"`
-			ContentType string `json:"Content-Type"`
-			Accept      string `json:"accept"`
-		} `json:"params"`
+		ResHeader interface{} `json:"res_header"`
+		ResBody   interface{} `json:"res_body"`
+		Params    interface{} `json:"params"`
 	} `json:"list"`
 }
